3d8d71d4e0db8407c010e0166504c0c613b73dee
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2023-05-16 15:02:28 -0400

    test: cleaned up tests and fixture files

--- spec/facades/giphy_facade_spec.rb
+++ spec/facades/giphy_facade_spec.rb
@@ -2,24 +2,22 @@ require 'rails_helper'
 
 RSpec.describe 'Giphy Facade' do
   describe 'Happy Path' do
-    context 'responses with gif data count > 0', :vcr do
-      before do
-        @response = GiphyFacade.mean_or_nice("If I throw a stick, will you leave?")
-      end 
-
+    context 'responses with gif data count > 0', :vcr do      
       it 'returns an array containing a hash with  url, title, rating key-value pairs' do
-        expect(@response).to be_a(Array)
-        expect(@response.count).to eq(1)
+        response = GiphyFacade.mean_or_nice("If I throw a stick, will you leave?")
         
-        expect(@response[0]).to be_a(Hash)
-        expect(@response[0].keys).to eq([:url, :title, :rating])
-        expect(@response[0][:url]).to be_a(String)
-        expect(@response[0][:title]).to be_a(String)
-        expect(@response[0][:rating]).to be_a(String)
+        expect(response).to be_a(Array)
+        expect(response.count).to eq(1)
+
+        expect(response[0]).to be_a(Hash)
+        expect(response[0].keys).to eq([:url, :title, :rating])
+        expect(response[0][:url]).to be_a(String)
+        expect(response[0][:title]).to be_a(String)
+        expect(response[0][:rating]).to be_a(String)
       end
     end
 
-    context "returned value adheres to the rating hierarchy, where R > PG/PG-13 > G", :vcr do    
+    context "response adheres to the rating hierarchy, where R > PG/PG-13 > G", :vcr do    
       it "returns a sample from of an 'R' rated gif if service response has one" do
         nsfw_response = GiphyFacade.mean_or_nice("nsfw")
         
