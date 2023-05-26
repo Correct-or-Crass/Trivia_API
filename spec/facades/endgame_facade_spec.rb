6254c807fb2a066cc2cef66d6861b9287d957ef2
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2023-05-26 17:46:06 -0400

    test: moved the variables into a let block

--- spec/facades/endgame_facade_spec.rb
+++ spec/facades/endgame_facade_spec.rb
@@ -3,21 +3,24 @@ require 'rails_helper'
 RSpec.describe 'Endgame Facade' do
   describe 'Happy Path' do
     context "the score determines the phrase type (insult or compliment)" do
+      let(:percentage_1) { EndgameFacade.score_percentage(2) }
+      let(:percentage_2) { EndgameFacade.score_percentage(4) }
+
+
       it "#score_percentage returns the percentage correct_answers; default total_questions = 5" do
-        percentage_1 = EndgameFacade.score_percentage(2)
-        percentage_2 = EndgameFacade.score_percentage(4)
 
         expect(percentage_1).to be_a Float
         expect(percentage_1).to be <= 60
         expect(percentage_2).to be >= 60
 
         #percentage_1 = (2.0/5.0 = 40.0); percentage_ 2 = (4.0/5.0 = 80.0)
-        expect(percentage_1).to eq(40.0)
-        expect(percentage_2).to eq(80.0)
+        expect(percentage_1).to be_within(0.01).of(40.0)
+        expect(percentage_2).to be_within(0.01).of(80.0)
       end    
 
-      # it "#find_phrase_type the phrase type " do
-      # end
+      it "returns the phrase type based on the percentage of correct answers" do
+
+      end
 
     end
   end
