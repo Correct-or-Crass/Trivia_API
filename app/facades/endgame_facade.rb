6da865ba1dafb8702ea6b547d5ea4aa01d1cd2e9
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2023-05-26 18:16:17 -0400

    stopping point: added notes for further development in facade

--- app/facades/endgame_facade.rb
+++ app/facades/endgame_facade.rb
@@ -1,7 +1,13 @@
 class EndgameFacade 
   #to do:
-  # make phrase service call with the type 
-  #pass phrase to giphy facade
+  #create class method that searchs for phrase and gif
+    #method will need to take an argument (rounds_won, total_rounds (optional))
+
+  #helper method will get the phrase
+    # make phrase service call with the #find_phrase_type 
+
+  # helper method will get the gif
+    #pass phrase to giphy facade
 
   def self.find_phrase_type(percentage)
     if percentage >= 60.0
@@ -11,7 +17,7 @@ class EndgameFacade
     end
   end
 
-  def self.score_percentage(correct_answers, total_questions = 5)
+  def self.score_percentage(rounds_won, total_rounds = 5)
     (correct_answers.to_f/total_questions.to_f) * 100
   end
 end
