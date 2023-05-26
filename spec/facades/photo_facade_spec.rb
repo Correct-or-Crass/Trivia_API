c05b64cc1ee5f83e7254b561e9054d5db2769f03
Bryan Flanagan <bflanagan78@yahoo.com>
2023-05-26 10:00:53 -0600

    test: Updates tests to include username, links. Tests passing

--- spec/facades/photo_facade_spec.rb
+++ spec/facades/photo_facade_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Photo Facade" do
-  describe 'Happy Path', :vcr do
+  describe 'Photo', :vcr do
     it 'makes an API call using the Unsplash gem' do
       response = PhotoFacade.photo("cat")
-      
+
       expect(response).to be_a Unsplash::Photo
       expect(response[:urls]).to be_a Unsplash::Client
       expect(response[:urls][:small]).to be_a String
       expect(response[:urls][:small]).to include ("https://images.unsplash.com/")
+      expect(response[:user]).to be_a Unsplash::Client
+      expect(response[:user][:username]).to be_a String
+      expect(response[:user][:links]).to be_a Unsplash::Client
+      expect(response[:user][:links][:self]).to be_a String
+      expect(response[:user][:links][:self]).to include("https://api.unsplash.com/users/#{response[:user][:username]}")
+
     end
   end
 end
