cd9d5d4d9d131c2316159be53f4fccc4bb11f4f1
Bryan Flanagan <bflanagan78@yahoo.com>
2023-05-26 09:52:01 -0600

    test: Adds tests for API call, tests passing

--- app/facades/photo_facade.rb
+++ app/facades/photo_facade.rb
@@ -1,7 +1,8 @@
 class PhotoFacade
   def self.photo(word)
      photo = Unsplash::Photo.search("#{word}")
-  #    require 'pry'; binding.pry
+     photo.sample
+    #  require 'pry'; binding.pry
   #    JSON.parse(response.body, symbolize_names: true)
   end
 end
--- spec/facades/photo_facade_spec.rb
+++ spec/facades/photo_facade_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Photo Facade" do
   describe 'Happy Path', :vcr do
-    it 'returns photo' do
+    it 'makes an API call using the Unsplash gem' do
       response = PhotoFacade.photo("cat")
-      # require 'pry'; binding.pry
-      # parse = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(response).to be_a Unsplash::Photo
+      expect(response[:urls]).to be_a Unsplash::Client
+      expect(response[:urls][:small]).to be_a String
+      expect(response[:urls][:small]).to include ("https://images.unsplash.com/")
     end
   end
 end
