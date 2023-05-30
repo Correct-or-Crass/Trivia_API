52d216a2db7573a9cc5da5e4569b12ec687d4a3b
Lacey Weaver <jlweave89@gmail.com>
2023-05-30 12:30:03 -0500

    Fix to the bug with poros

--- app/facades/word_facade.rb
+++ app/facades/word_facade.rb
@@ -1,15 +1,17 @@
 class WordFacade
   def self.find_word
     response = WordService.word_search
-
-    response[:data][:attributes][:word]
+# require 'pry'; binding.pry
+    response[:data][0][:attributes][:word]
    
   end
 
   def self.multi_word_search(num)
-    responses = WordService.multi_word(num)    
-    responses.map do |response|
-      Word.new(response)
+    responses = WordService.multi_word(num) 
+    words = [] 
+    responses[:data].select do |response|
+      words << response[:attributes][:word]
     end
+    words
   end
 end
