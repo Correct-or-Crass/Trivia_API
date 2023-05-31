8f3da013f1af6b0bd75787d919ee3fef3280ddf2
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2023-05-30 21:37:40 -0400

    feat: RandomPhraseService adjusted odd spacing

--- app/services/random_phrase_service.rb
+++ app/services/random_phrase_service.rb
@@ -1,8 +1,8 @@
 require "./lib/modules/serviceable.rb"
 
 class RandomPhraseService
-    def self.generate_phrase(type)
-        response = Serviceable.phrases_words_connection.get("api/v1/#{type}")
-        Serviceable.parse_json(response) 
-    end
+  def self.generate_phrase(type)
+    response = Serviceable.phrases_words_connection.get("api/v1/#{type}")
+    Serviceable.parse_json(response) 
+  end
 end
