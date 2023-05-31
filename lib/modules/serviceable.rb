77022ba69eaa8932089ab1bf675e9b7f2a527694
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2023-05-30 20:59:01 -0400

    feat: Serviceable module-removed root Faraday as not used at the moment

--- lib/modules/serviceable.rb
+++ lib/modules/serviceable.rb
@@ -3,10 +3,6 @@ module Serviceable
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def self.conn(root)
-    Faraday.new(root)
-  end
-
   def self.phrases_words_connection
     Faraday.new("https://shielded-headland-00998.herokuapp.com")
   end
