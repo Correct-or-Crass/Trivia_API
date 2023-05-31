d4730ee7c8ed2b31f29e4a437b301ac181866c32
Sergio <sergio.d.azcona@gmail.com>
2023-05-31 10:31:32 -0400

    Update word_facade_spec.rb-responses
    minor spelling correction

--- spec/facades/word_facade_spec.rb
+++ spec/facades/word_facade_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe 'Word Facade' do
   describe 'Happy Path' do
     context 'responses with word facade', :vcr do
-      it 'repsonses with a random word' do
+      it 'responses with a random word' do
         response = WordFacade.find_word
 
         expect(response).to be_a String
         expect(response.split.count).to eq 1
       end
       
-      it 'repsonses with multiple random words' do
+      it 'responses with multiple random words' do
         response = WordFacade.multi_word_search(4)
         
         expect(response).to be_an Array
