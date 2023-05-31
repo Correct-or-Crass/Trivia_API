cd4fb7d9383a9acbd477540070e09cea92f7d449
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2023-05-30 20:57:36 -0400

    feat: module Serviceable#root_with_credentials inplemented and working; #parse_jason implemented and working

--- app/services/giphy_service.rb
+++ app/services/giphy_service.rb
@@ -3,22 +3,13 @@ require "./lib/modules/serviceable.rb"
 class GiphyService
   include Serviceable
   def self.gif_search(phrase)
-    limit = 20  # setting as default for now;
-    rating = "r" 
-
-    root = Serviceable.conn("https://api.giphy.com")
+    root = Serviceable.root_with_credentials("https://api.giphy.com", "api_key", "giphy_api_key")
 
     response = root.get("/v1/gifs/search") do |search|
       search.params["q"] = phrase
-      search.params["limit"] = limit
-      search.params["rating"] = rating
-      search.params["api_key"] = ENV["giphy_api_key"]
+      search.params["limit"] = 20   # setting as default for now;
+      search.params["rating"] = "r"
     end
     Serviceable.parse_json(response) 
   end
-
-  # def self.root_with_credentials
-  #   Serviceable.conn("https://api.giphy.com") do |root|
-  #   end
-  # end
 end
--- lib/modules/serviceable.rb
+++ lib/modules/serviceable.rb
@@ -10,4 +10,10 @@ module Serviceable
   def self.phrases_words_connection
     Faraday.new("https://shielded-headland-00998.herokuapp.com")
   end
+
+  def self.root_with_credentials(root, param_title, env_key)
+    Faraday.new(root) do |root|
+      root.params[param_title] = ENV[env_key]
+    end
+  end
 end
