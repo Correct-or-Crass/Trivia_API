858e871dfa426d80749fd62b3710ff10e8df9c2f
Bryan Flanagan <bflanagan78@yahoo.com>
2023-06-01 08:14:58 -0600

    refactor: Updates test for photofacade, tests passing

--- app/facades/photo_facade.rb
+++ app/facades/photo_facade.rb
@@ -2,7 +2,5 @@ class PhotoFacade
   def self.photo(word)
      photo = Unsplash::Photo.search("#{word}")
      photo.sample.to_json
-    #  require 'pry'; binding.pry
-  #    JSON.parse(response.body, symbolize_names: true)
   end
 end
--- spec/facades/photo_facade_spec.rb
+++ spec/facades/photo_facade_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe "Photo Facade" do
       expect(parse).to be_a Hash
       expect(parse).to have_key (:attributes)
       expect(parse[:attributes]).to be_a Hash
-      # require 'pry'; binding.pry
       expect(parse[:attributes]).to have_key (:table)
       expect(parse[:attributes][:table]).to be_a Hash
       expect(parse[:attributes][:table]).to have_key (:id)
@@ -19,15 +18,16 @@ RSpec.describe "Photo Facade" do
       expect(parse[:attributes][:table][:urls]).to have_key (:raw)
       expect(parse[:attributes][:table][:urls][:raw]).to be_a String
       expect(parse[:attributes][:table][:urls][:raw]).to include "https://images.unsplash.com/photo"
-      expect(parse[:attributes][:table][:urls]).to have_key (:user)
-      expect(parse[:attributes][:table][:urls][:user]).to be_a Hash
-      expect(parse[:attributes][:table][:urls][:user]).to have_key (:name)
-      expect(parse[:attributes][:table][:urls][:user][:name]).to be_a String
-      expect(parse[:attributes][:table][:urls][:user]).to have_key (:links)
-      expect(parse[:attributes][:table][:urls][:user][:links]).to be_a Hash
-      expect(parse[:attributes][:table][:urls][:user][:links]).to have_key (:self)
-      expect(parse[:attributes][:table][:urls][:user][:links]).to be_a String
-      expect(parse[:attributes][:table][:urls][:user][:links]).to include "https://api.unsplash.com/users"
+  
+      expect(parse[:attributes][:table]).to have_key (:user)
+      expect(parse[:attributes][:table][:user]).to be_a Hash
+      expect(parse[:attributes][:table][:user]).to have_key (:name)
+      expect(parse[:attributes][:table][:user][:name]).to be_a String
+      expect(parse[:attributes][:table][:user]).to have_key (:links)
+      expect(parse[:attributes][:table][:user][:links]).to be_a Hash
+      expect(parse[:attributes][:table][:user][:links]).to have_key (:self)
+      expect(parse[:attributes][:table][:user][:links][:self]).to be_a String
+      expect(parse[:attributes][:table][:user][:links][:self]).to include "https://api.unsplash.com/users"
 
 
 
