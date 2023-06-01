f22b29901c00bdd0f66641caebc234f7c99a546f
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2023-05-31 22:39:18 -0400

    removed coverage and log files from staging

--- spec/facades/word_facade_spec.rb
+++ spec/facades/word_facade_spec.rb
@@ -5,9 +5,18 @@ RSpec.describe 'Word Facade' do
     context 'responses with word facade', :vcr do
       it 'repsonses with a random word' do
         response = WordFacade.find_word
+
+        expect(response).to be_a String
+        expect(response.split.count).to eq 1
+        require 'pry'; binding.pry
       end
+      
       it 'repsonses with multiple random words' do
         response = WordFacade.multi_word_search(4)
+        
+        expect(response).to be_an Array
+        expect(response.count).to eq 4
+        expect(response[0]).to eq response[0].capitalize
       end
     end
   end
