a5cb15ef9d7997be51c40d3af861ea0244e14505
Bryan Flanagan <bflanagan78@yahoo.com>
2023-06-14 11:34:39 -0600

    refactor: Update service to use Serviceable module

--- app/services/word_service.rb
+++ app/services/word_service.rb
@@ -1,3 +1,4 @@
+require './lib/modules/serviceable'
 class WordService
   def self.conn
    response =  Faraday.new("https://shielded-headland-00998.herokuapp.com")
@@ -5,12 +6,15 @@ class WordService
 
   def self.word_search
     response = conn.get("/api/v1/words?amount=")
-    JSON.parse(response.body, symbolize_names: true) 
+    # JSON.parse(response.body, symbolize_names: true) 
+    require 'pry'; binding.pry
+    Serviceable.parse_json(response)
   end
 
   def self.multi_word(num=4)
     response = conn.get("/api/v1/words?amount=#{num}")
-    
-    JSON.parse(response.body, symbolize_names: true)
+    Serviceable.parse_json(response)
+
+    # JSON.parse(response.body, symbolize_names: true)
   end
 end
