62b77c20154012d6188a5e29fdb88ede0f8aa93c
Bryan Flanagan <bflanagan78@yahoo.com>
2023-06-14 14:46:41 -0600

    refactor: Updates photo facade to remove single photo method, refactors photos method. Tests passing

--- app/facades/photo_facade.rb
+++ app/facades/photo_facade.rb
@@ -1,9 +1,10 @@
 class PhotoFacade
   def self.photos(word)
     @photo = Unsplash::Photo.search("#{word}")
-  end
-
-  def self.single_photo
-    @photo.sample.to_json
+    pr = JSON.parse(@photo.sample.to_json, symbolize_names: true)
+    image_url = pr[:attributes][:table][:urls][:raw]
+    creator_name = pr[:attributes][:table][:user][:username]
+    creator_link = pr[:attributes][:table][:links][:self]
+    photo = {"image_url" => image_url, "creator_name" => creator_name, "creator_link" => creator_link}
   end
 end
--- spec/facades/photo_facade_spec.rb
+++ spec/facades/photo_facade_spec.rb
@@ -2,42 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Photo Facade' do
   describe 'Photo', :vcr do
-    it 'makes an API call using the Unsplash gem' do
+    it 'makes an API call using the Unsplash gem and returns a photo with credits' do
       response = PhotoFacade.photos("cat")
       response_json = response.to_json
       parse = JSON.parse(response_json, symbolize_names: true)
-
-      expect(parse).to be_a Array
-      parse.each do |photo|
-        expect(photo).to be_a Hash
-      end
-    end
-    it 'returns a single Unsplash photo' do
-      single_response = PhotoFacade.single_photo
-      single_photo = JSON.parse(single_response, symbolize_names: true)
       
-      expect(single_photo).to be_a Hash
-      expect(single_photo).to have_key (:attributes)
-      expect(single_photo[:attributes]).to be_a Hash
-      expect(single_photo[:attributes]).to have_key (:table)
-      expect(single_photo[:attributes][:table]).to be_a Hash
-      expect(single_photo[:attributes][:table]).to have_key (:id)
-      expect(single_photo[:attributes][:table][:id]).to be_a String
-      expect(single_photo[:attributes][:table]).to have_key (:urls)
-      expect(single_photo[:attributes][:table][:urls]).to be_a Hash
-      expect(single_photo[:attributes][:table][:urls]).to have_key (:raw)
-      expect(single_photo[:attributes][:table][:urls][:raw]).to be_a String
-      expect(single_photo[:attributes][:table][:urls][:raw]).to include "https://images.unsplash.com/photo"
-      
-      expect(single_photo[:attributes][:table]).to have_key (:user)
-      expect(single_photo[:attributes][:table][:user]).to be_a Hash
-      expect(single_photo[:attributes][:table][:user]).to have_key (:name)
-      expect(single_photo[:attributes][:table][:user][:name]).to be_a String
-      expect(single_photo[:attributes][:table][:user]).to have_key (:links)
-      expect(single_photo[:attributes][:table][:user][:links]).to be_a Hash
-      expect(single_photo[:attributes][:table][:user][:links]).to have_key (:self)
-      expect(single_photo[:attributes][:table][:user][:links][:self]).to be_a String
-      expect(single_photo[:attributes][:table][:user][:links][:self]).to include "https://api.unsplash.com/users"
+      expect(parse).to be_a Hash
+      expect(parse).to have_key(:image_url)
+      expect(parse[:image_url]).to be_a String
+      expect(parse[:image_url]).to include "https://images.unsplash.com/"
+      expect(parse).to have_key(:creator_name)
+      expect(parse[:creator_name]).to be_a String
+      expect(parse).to have_key(:creator_link)
+      expect(parse[:creator_link]).to be_a String
+      expect(parse[:creator_link]).to include "https://api.unsplash.com/photos/"
     end
   end
 end
