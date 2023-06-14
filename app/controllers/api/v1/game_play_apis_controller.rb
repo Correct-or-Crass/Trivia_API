77c562296d6098f15e978d78dbc29e4ac774141e
Bryan Flanagan <bflanagan78@yahoo.com>
2023-06-14 11:44:12 -0600

    feat: Adds index to return photos with choices, untested

--- app/controllers/api/v1/game_play_apis_controller.rb
+++ app/controllers/api/v1/game_play_apis_controller.rb
@@ -2,7 +2,10 @@
 
 class Api::V1::GamePlayApisController < ApplicationController
   def index 
-    @word = WordFacade.find_word
+    # @word = WordFacade.find_word
     # require 'pry'; binding.pry
+    @answers = WordFacade.multi_word_search(num)
+    @photo = PhotoFacade.photos(@answer[0])
+    render json: GamePlaySerializer.new(@photo, @answers[0], @answers), status:200
   end
 end
