7be31442004d96c43a9c6a9a16915a6c56f29f66
Bryan Flanagan <bflanagan78@yahoo.com>
2023-06-14 15:27:19 -0600

    refactor: Updates service to include Serviceable module

--- app/services/word_service.rb
+++ app/services/word_service.rb
@@ -6,15 +6,11 @@ class WordService
 
   def self.word_search
     response = conn.get("/api/v1/words?amount=")
-    # JSON.parse(response.body, symbolize_names: true) 
-    require 'pry'; binding.pry
     Serviceable.parse_json(response)
   end
 
   def self.multi_word(num=4)
     response = conn.get("/api/v1/words?amount=#{num}")
     Serviceable.parse_json(response)
-
-    # JSON.parse(response.body, symbolize_names: true)
   end
 end
