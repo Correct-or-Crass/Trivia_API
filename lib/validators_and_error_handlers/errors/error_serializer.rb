334b9f3a0cd83235e2f1b7102f2c1d9dae85dce3
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2023-08-02 14:52:15 -0400

    ErrorSerializer: adding as it is called in the ErrorHandler

--- lib/validators_and_error_handlers/errors/error_serializer.rb
+++ lib/validators_and_error_handlers/errors/error_serializer.rb
@@ -3,17 +3,17 @@ module ValidatorsAndErrorHandlers
     class ErrorSerializer
       def self.bad_request(detail_message, status_code, source_info)
         {
-            errors:
-            [ 
-              {
-              title: "Bad Request",
-              detail: detail_message,
-              status: status_code.to_s, 
-              source: {
-                      pointer: source_info,
-                      }
-              } 
-            ]
+          errors:
+          [ 
+            {
+            title: "Bad Request",
+            detail: detail_message,
+            status: status_code.to_s, 
+            source: {
+                    pointer: source_info,
+                    }
+            } 
+          ]
         }
       end
     end
